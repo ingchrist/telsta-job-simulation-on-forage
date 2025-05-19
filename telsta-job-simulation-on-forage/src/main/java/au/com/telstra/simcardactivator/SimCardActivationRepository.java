9a4d3509ebf204522fa22cacb6cde2991c8286db
ingchrist <127639176+ingchrist@users.noreply.github.com>
2025-05-19 10:57:41 +0000

    ok

--- telsta-job-simulation-on-forage/src/main/java/au/com/telstra/simcardactivator/SimCardActivationRepository.java
+++
@@ -1,6 +0,0 @@
-package au.com.telstra.simcardactivator;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SimCardActivationRepository extends JpaRepository<SimCardActivation, Long> {
-}
